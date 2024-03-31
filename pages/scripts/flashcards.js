286133914ebf37b511d7e602b3a1be80395a5a91
ryan-saloma <ryan.saloma@gmail.com>
2024-03-30 20:43:45 -0400

    skeleton code for btn-addRow event listener

--- pages/scripts/flashcards.js
+++ pages/scripts/flashcards.js
@@ -1,4 +1,8 @@
-let addRow = Document.querySelector('.btn-addRow');
-addRow.addEventListener('mousedown', function () {
-    alert('You pressed the button');
-})
+// Get the button element
+const addButton = document.querySelector('.btn-addRow');
+
+// Add a click event listener to the button
+addButton.addEventListener('click', function (event) {
+    // Your click event handler code goes here
+    console.log('Button clicked!');
+});
