03ea1e7ea39f05a9599d6edd01ace74a08afbe5d
ryan-saloma <ryan.saloma@gmail.com>
2024-03-30 22:06:02 -0400

    added event handler for adding card to collection

--- pages/scripts/flashcards.js
+++ pages/scripts/flashcards.js
@@ -1,8 +1,19 @@
 // Get the button element
+const flashcardContainer = document.querySelector('.new-flashcard-form');
+let hiddenFlashcard = document.querySelector('.flashcard--hidden');
 const addButton = document.querySelector('.btn-addRow');
 
 // Add a click event listener to the button
+// need to 1) make invisible card visible, 
+// 2) generate new invisible card, 
+// 3) set new-flashcard-form to old + 25%
 addButton.addEventListener('click', function (event) {
-    // Your click event handler code goes here
-    console.log('Button clicked!');
+    const copiedFlashcard = hiddenFlashcard.cloneNode(true);
+    let currentHeight = flashcardContainer.clientHeight;
+    console.log(currentHeight);
+    flashcardContainer.style.height = `${currentHeight + 175}px`;
+    hiddenFlashcard.classList.add('flashcard--visible');
+    hiddenFlashcard.classList.remove('flashcard--hidden');
+    hiddenFlashcard = copiedFlashcard;
+    flashcardContainer.appendChild(hiddenFlashcard);
 });
